c3798cccf779a166100fa0fab8ff8c3d1b32659b
Leandro DCI <leandro.frigerio@digitalcareerinstitute.org>
2021-04-15 21:01:25 +0200

    fix: minimize

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     },
     devtool: 'source-map',
     optimization: {
+        minimize: true,
         minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
     },
     module: {
