52a66eef11f0ba975d6e49647a2e48ff42636418
Leandro DCI <leandro.frigerio@digitalcareerinstitute.org>
2021-04-15 19:53:53 +0200

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,14 @@ module.exports = {
                     'sass-loader',
                     {
                         loader: 'postcss-loader',
+                        options: {
+                            // `postcssOptions` is needed for postcss 8.x;
+                            // if you use postcss 7.x skip the key
+                            postcssOptions: {
+                                // postcss plugins, can be exported to postcss.config.js
+                                plugins: [require('autoprefixer')],
+                            },
+                        },
                     },
                 ],
             },
